26440d71f85cbef73f4bf9589845f0e6658d2298
rainydaysavings <mail@eliseuama.ro>
2021-03-24 16:44:42 +0000

    Need to switch points and reverse those inbetween.

--- src/core/Core.java
+++ src/core/Core.java
@@ -1,51 +1,14 @@
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Core {
-
-    public static void graphRandom(Graph g, int n, int min, int max) {
-        for (int i = 0; i < n; ++i) {
-            int x = (int) (Math.random() * (max - min + 1) + min);
-            int y = (int) (Math.random() * (max - min + 1) + min);
-
-            Point p = new Point(x, y);
-
-            // check if already in
-            if (!g.contains(p)) {
-                // add if not in
-                g.addPoint(p);
-
-                System.out.println("x: " + p.getX() + "\t" + "y: " + p.getY() + " " + g.nodes + "\t\n" + "indice " + i);
-            } else {
-                System.out.println("ALREADY IN");
-            }
-        }
-    }
-/*
-    public static void linkRandom(Graph g) {
-        for (int i = 0; i < g.getSize(); ++i) {
-            int randA = (int) (Math.random() * (g.getSize()));
-            int randB = (int) (Math.random() * (g.getSize()));
-
-            Node a = g.nodes.get(randA);
-            Node b = g.nodes.get(randB);
-
-            if (!g.containsLink(a, b)) {
-                g.addLink(a, b);
-                System.out.println(g.nodes.get(randA) + " connected to " + g.nodes.get(randB));
-            }
-        }
-    }
-*/
-
     public static void nearest(Graph g) {
         int randA = (int) (Math.random() * (g.getSize()));
         System.out.println("começa em " + randA);
         Node a = g.nodes.get(randA);
 
         g.find_nearest(a);
-        //System.out.println("vai para " +randA);
-
     }
 
     public static void main(String[] args) {
@@ -53,19 +16,36 @@ public class Core {
         int n, min, max;
 
         // ask stuff
-        System.out.println("Quantos pontos? ");
-        n = in.nextInt();
         System.out.println("Limite inferior e superior? ");
         min = in.nextInt();
         max = in.nextInt();
 
+        int range = Math.abs(max) - Math.abs(min);
+        int n_max = range * range;
+
+        System.out.println("Quantos pontos? Inserir no máximo " + n_max + ".");
+        n = in.nextInt();
+
+        if (n > n_max) {
+            System.out.println("Try again...");
+            n = in.nextInt();
+        }
+
         Graph g = new Graph();
-        graphRandom(g, n, min, max);
-
-        //linkRandom(g);
-
+        g.graphRandom(n, 0, min, max);
         nearest(g);
 
+        for (int i = 0; i < g.nearest.size(); ++i) {
+            System.out.println(g.nearest.get(i).toString());
+        }
+
+
+        System.out.println();
+
+        g.toExchange();
+        for (int i = 0; i < g.candidates.size(); ++i) {
+            System.out.println(Arrays.toString(g.candidates.get(i).toArray()));
+        }
     }
 }
 
--- src/core/Graph.java
+++ src/core/Graph.java
@@ -13,16 +13,38 @@ class Node {
         //this.adj = new LinkedList<Node>();
         this.point = p;
     }
+
+
 }
 
 public class Graph {
     public LinkedList<Node> nodes;
-    public LinkedList<Node> nearest;
-    public LinkedList<LinkedList<Node>> candidates;
+    public LinkedList<Point2D> nearest;
+    public LinkedList<LinkedList<Point2D>> candidates;
 
     public Graph() {
-        nodes = new LinkedList<Node>();
-        nearest = new LinkedList<Node>();
+        nodes = new LinkedList<>();
+        nearest = new LinkedList<>();
+        candidates = new LinkedList<>();
+    }
+
+    public void graphRandom(int n, int i, int min, int max) {
+        if (i == n) return;
+
+        int x = (int) (Math.random() * (max - min + 1) + min);
+        int y = (int) (Math.random() * (max - min + 1) + min);
+
+        Point p = new Point(x, y);
+
+        // check if already in
+        if (!contains(p)) {
+            System.out.println("[index: " + i + "] " + "x: " + p.getX() + "\t" + "y: " + p.getY());
+            addPoint(p);
+            ++i;
+        } else {
+            System.out.println("ALREADY IN");
+        }
+        graphRandom(n, i, min, max);
     }
 
     /**
@@ -30,10 +52,10 @@ public class Graph {
      * 1 --> clockwise
      * 2 --> counter clockwise
      */
-    public int checkOrientation(Node n1_start, Node n1_end, Node wildcard) {
-        return (int) ((n1_end.point.getY() - n1_start.point.getY()) *
-                (wildcard.point.getX() - n1_end.point.getX()) *
-                (wildcard.point.getY() - n1_end.point.getY()));
+    public int checkOrientation(Point2D n1_start, Point2D n1_end, Point2D wildcard) {
+        return (int) ((n1_end.getY() - n1_start.getY()) *
+                (wildcard.getX() - n1_end.getX()) *
+                (wildcard.getY() - n1_end.getY()));
     }
 
     static boolean onSegment(Point2D p, Point2D q, Point2D r) {
@@ -41,7 +63,7 @@ public class Graph {
                 q.getY() <= Math.max(p.getY(), r.getY()) && q.getY() >= Math.min(p.getY(), r.getY());
     }
 
-    boolean checkIntersect(Node s1_start, Node s1_end, Node s2_start, Node s2_end) {
+    boolean checkIntersect(Point2D s1_start, Point2D s1_end, Point2D s2_start, Point2D s2_end) {
         int o1 = checkOrientation(s1_start, s1_end, s2_start);
         int o2 = checkOrientation(s1_start, s1_end, s2_end);
         int o3 = checkOrientation(s2_start, s2_end, s1_start);
@@ -50,20 +72,62 @@ public class Graph {
         if (o1 != o2 && o3 != o4)
             return true;
 
-        if (o1 == 0 && onSegment(s1_start.point, s2_start.point, s1_end.point)) return true;
+        if (o1 == 0 && onSegment(s1_start, s2_start, s1_end)) return true;
 
         // p1, q1 and q2 are colinear and q2 lies on segment p1q1
-        if (o2 == 0 && onSegment(s1_start.point, s2_end.point, s1_end.point)) return true;
+        if (o2 == 0 && onSegment(s1_start, s2_end, s1_end)) return true;
 
         // p2, q2 and p1 are colinear and p1 lies on segment p2q2
-        if (o3 == 0 && onSegment(s2_start.point, s1_start.point, s2_end.point)) return true;
+        if (o3 == 0 && onSegment(s2_start, s1_start, s2_end)) return true;
 
         // p2, q2 and q1 are colinear and q1 lies on segment p2q2
-        return o4 == 0 && onSegment(s2_start.point, s1_end.point, s2_end.point);
+        return o4 == 0 && onSegment(s2_start, s1_end, s2_end);
+    }
+
+    // reverse bs need to work here
+    LinkedList<Point2D> checkCase(Point2D s1_start, Point2D s1_end, Point2D s2_start, Point2D s2_end) {
+        LinkedList<Point2D> candidate = new LinkedList<>();
+        candidate = (LinkedList<Point2D>) nearest.clone();
+
+        // if s1 -> e2 and s2 -> e1 already exist do second case
+        if ((nearest.indexOf(s1_start) == (nearest.indexOf(s2_start) - 1)) && nearest.indexOf(s1_end) == (nearest.indexOf(s2_end) - 1)) {
+            // switch indexes
+            int to_advance_index = candidate.indexOf(s1_end);
+            int to_previous_index = candidate.indexOf(s2_start);
+            Point2D advance = candidate.get(to_advance_index);
+            Point2D previous = candidate.get(to_previous_index);
+            candidate.remove(to_advance_index);
+            candidate.remove(to_previous_index);
+            candidate.add(to_advance_index, advance);
+            candidate.add(to_previous_index, previous);
+        }
+
+        return candidate;
+    }
+
+    int genCandidate(int i) {
+        Point2D s1, s2, e1, e2;
+        s1 = nearest.get(i);
+
+        for (int j = i; j < nearest.size() - 3; ++j) {
+            e1 = nearest.get(j + 1);
+            s2 = nearest.get(j + 2);
+            e2 = nearest.get(j + 3);
+
+            if (checkIntersect(s1, e1, s2, e2)) {
+                candidates.addLast(checkCase(s1, e1, s2, e2));
+            }
+        }
+
+        return ++i;
     }
 
     public void toExchange() {
+        int i = 0;
 
+        while (i < nearest.size()) {
+            i = genCandidate(i);
+        }
     }
 
     public int getSize() {
@@ -73,12 +137,6 @@ public class Graph {
     public void addPoint(Point p) {   //add a node with a point
         Node n = new Node(p);
         nodes.addLast(n);
-    }
-
-
-    public void addPointNearest(Point p) {   //add a node with a point
-        Node n = new Node(p);
-        nearest.addLast(n);
     }
 
     public boolean contains(Point thisp) {
@@ -96,13 +154,14 @@ public class Graph {
     }
 
     public void find_nearest(Node a) {
-
+        nearest.addLast(a.point);
         a.visited = true;
-        int flag = 0;
-        double distance = 0;                               ///
-        Node proximo = null;                               ///
+        Node next = null;
+
         int i = 0;
-        int elimina = 0;
+        int nextCandidate = 0;
+        int flag = 0;
+        double distance = 0;
 
         while (i < nodes.size()) {
             Node T = nodes.get(i);
@@ -110,33 +169,26 @@ public class Graph {
                 if (!T.visited) {                           //ainda nao foi visitado
                     if (flag == 0) {
                         flag = 1;
-                        elimina = i;
+                        nextCandidate = i;
                         distance = a.point.distance(T.point);
-                        //System.out.println("yooo " +T);
-                        proximo = nodes.get(i);
+                        next = nodes.get(i);
                     } else {
                         if (a.point.distance(T.point) < distance) {
                             distance = a.point.distance(T.point);
-                            proximo = nodes.get(i);
-                            elimina = i;
-                            //System.out.println("é o indice " +i);
+                            next = nodes.get(i);
+                            nextCandidate = i;
                         }
                     }
                 }
-                //System.out.println("yooo" +T);
             }
             ++i;
         }
 
         if (flag == 1) {
-            //addPointNearest(proximo.p);
-            System.out.println("vai para " + elimina);
-            find_nearest(proximo);
-        }/*
-        else{
-            addLink(a, inicial);
-        }*/
+            System.out.println("vai para " + nextCandidate);
+            find_nearest(next);
+        } else {
+            nearest.addLast(nearest.get(0));
+        }
     }
-
-
 }
